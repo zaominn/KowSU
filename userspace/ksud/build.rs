ebe5000dd64f175ec808442947a39ab87bd46db3
zaominn <205947332+zaominn@users.noreply.github.com>
2026-09-14 10:06:03 +0100

    fix: use KowSU application identity

--- userspace/ksud/build.rs
+++ userspace/ksud/build.rs
@@ -228,7 +228,7 @@ fn main() {
         }
     };
     if env::var("KSU_PACKAGE_NAME").is_err() {
-        println!("cargo:rustc-env=KSU_PACKAGE_NAME=me.weishu.kernelsu");
+        println!("cargo:rustc-env=KSU_PACKAGE_NAME=com.zaominn.kowsu");
     }
     println!("cargo:rustc-env=VERSION_CODE={code}");
     println!("cargo:rustc-env=VERSION_NAME={name}");
