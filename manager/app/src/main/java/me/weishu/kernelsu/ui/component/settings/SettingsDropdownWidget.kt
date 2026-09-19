74855a7208aca1cde25c554e545f1910d39bc296
zaominn <205947332+zaominn@users.noreply.github.com>
2026-09-19 16:52:22 -0400

    fix: use selectable SUSFS dropdown items

--- manager/app/src/main/java/me/weishu/kernelsu/ui/component/settings/SettingsDropdownWidget.kt
+++ manager/app/src/main/java/me/weishu/kernelsu/ui/component/settings/SettingsDropdownWidget.kt
@@ -3,11 +3,11 @@ package me.weishu.kernelsu.ui.component.settings
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.offset
 import androidx.compose.material3.DropdownMenuGroup
-import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.DropdownMenuPopup
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.material3.MenuDefaults
+import androidx.compose.material3.SelectableDropdownMenuItem
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -81,7 +81,7 @@ fun SettingsDropdownWidget(
 
                         // Utilize the selectable variation of DropdownMenuItem
                         // MenuDefaults.itemShape(index, count) automatically handles the shapes
-                        DropdownMenuItem(
+                        SelectableDropdownMenuItem(
                             selected = isSelected,
                             onClick = {
                                 onChoiceChange(index)
